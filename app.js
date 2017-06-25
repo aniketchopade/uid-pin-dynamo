3aa450edf8e20ed40ec15dad2f71fdb73e9ae2f7
Chopade <aniket.c@tcs.com>
2017-06-24 23:37:31 -0400

    initial claudia update

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var ApiBuilder = require('claudia-api-builder'),
+var ApiBuilder = require('claudia-api-builder');
+var AWS = require('aws-sdk');
+
 api = new ApiBuilder();
 module.exports = api;
 
 api.get('/hello', function () {
   return 'hello world';
+});
+
+api.get('/greet', function (request) {
+  var superb = require('superb');
+  return request.queryString.name + ' is ' + superb();
 });
