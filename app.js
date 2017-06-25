cc679863b2d8e0c3557a565db59692fd9647f6f2
Chopade <aniket.c@tcs.com>
2017-06-25 13:10:34 -0400

    success read

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 var ApiBuilder = require('claudia-api-builder');
 var AWS = require('aws-sdk');
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var table = "pin";
+var pin = "12345";
+var params = {
+    TableName: table,
+    Key:{
+        "pin": pin,
+    }
+};
 
 api = new ApiBuilder();
 module.exports = api;
 
-api.get('/hello', function () {
-  return 'hello world';
-});
 
 api.get('/greet', function (request) {
-  var superb = require('superb');
-  return request.queryString.name + ' is ' + superb();
+  console.log("i am in greet method");
+  console.log("JSON stringify" + JSON.stringify(docClient));
+  
+    AWS.config.update({
+        region: "us-east-1",
+        endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+    });
+
+    return new Promise ( (resolve, reject ) => {
+        docClient.get(params, (err, data)=>{
+            if (err) { 
+                    console.log("failed!");
+                    resolve(err) }
+            else { 
+                console.log("passed!");
+                resolve(data)};
+        })
+    });
 });
